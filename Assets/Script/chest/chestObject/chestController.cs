4f922241d4f4c52f9616869b4f3428882c9a9634
Semi-Otto <ifanfair04@gmail.com>
2022-01-21 12:51:59 -0500

    Update chestController.cs
    added line to return weaponDmg on the other chest

--- Assets/Script/chest/chestObject/chestController.cs
+++ Assets/Script/chest/chestObject/chestController.cs
@@ -17,6 +17,7 @@ public class chestController : MonoBehaviour
         {
             drop.randomValue();//creates a random value used for the dropped function
             weaponName = drop.dropped("consumable.json",4);//runs the dropped function giving the file name and list size
+            weaponDmg = drop.dmgReturn(weaponName);
         }
 
         if(gameObject.tag == "firearms")//checks the tag of the chest
@@ -30,12 +31,14 @@ public class chestController : MonoBehaviour
         {
             drop.randomValue();//creates a random value used for the dropped function
             weaponName = drop.dropped("melee.json",6);//runs the dropped function giving the file name and list size
+            weaponDmg = drop.dmgReturn(weaponName);
         }
 
         if(gameObject.tag == "goku")//checks the tag of the chest
         {
            drop.randomValue();//creates a random value used for the dropped function
            weaponName = drop.dropped("goku.json",7);//runs the dropped function giving the file name and list size
+            weaponDmg = drop.dmgReturn(weaponName);
         }
     }
 }
