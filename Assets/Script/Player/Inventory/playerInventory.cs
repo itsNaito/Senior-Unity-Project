7522fa87b5a4ee4e2752b4887ec2116e898be1b7
Semi-Otto <ifanfair04@gmail.com>
2022-01-24 13:35:42 -0500

    updated
    created a visual inventory that the player can open and interact. Currently the player can only open and close the inventory

--- Assets/Script/Player/Inventory/playerInventory.cs
+++ Assets/Script/Player/Inventory/playerInventory.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 public class playerInventory : MonoBehaviour
 {
+    private bool openinv;
+    public Canvas invCanvas;
     public List<string> inventoryName = new List<string>();//stores the items the player gains from chest
     public List<int> inventoryDmg = new List<int>();
     public void insertInventory(string weapon, int dmg)//adds the item to the player inventory
@@ -11,6 +14,25 @@ public class playerInventory : MonoBehaviour
         inventoryName.Add(weapon);//adds the name of the item to the inventory list
         inventoryDmg.Add(dmg);
         universalInventory.newItem(weapon, dmg);
+    }
+    void Update()
+    {
+        openInventory();
+    }
+    void openInventory()
+    {
+        Canvas render = invCanvas.GetComponent<Canvas>();
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            if(render.enabled == true)
+            {
+                render.enabled = false;
+            }
+            else if(render.enabled == false)
+            {
+                render.enabled = true;
+            }
+        }
     }
 }
 
